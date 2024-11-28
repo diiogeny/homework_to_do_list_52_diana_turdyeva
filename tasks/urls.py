ff21e230bff913f7bbffe53ce203b91fb863e183
Диана Турдыева <dianaturdyevar@gmail.com>
2024-11-28 12:58:04 +0100

    Добавлены изменения

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),               # Список задач
-    path('<int:pk>/', views.task_detail, name='task_detail'),  # Детальная информация о задаче
+    path('', views.task_list, name='task_list'),
+    path('add/', views.add_task, name='add_task'),
+    path('<int:pk>/', views.task_detail, name='task_detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -8,3 +9,13 @@ def task_list(request):
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'tasks/task_detail.html', {'task': task})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
