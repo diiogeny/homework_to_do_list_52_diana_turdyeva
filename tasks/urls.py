4c875d3214b2fed24020e65d97dda66d6f17e418
Диана Турдыева <dianaturdyevar@gmail.com>
2024-12-05 12:07:43 +0100

    Добавлены изменения в проект

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
     path('<int:pk>/', views.task_detail, name='task_detail'),
+    path('edit/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
-
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
@@ -11,11 +11,30 @@ def task_detail(request, pk):
     return render(request, 'tasks/task_detail.html', {'task': task})
 
 def add_task(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('task_list')
     else:
         form = TaskForm()
-    return render(request, 'tasks/add_task.html', {'form': form})
+    return render(request, 'tasks/add_edit_task.html', {'form': form, 'title': 'Добавить задачу'})
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/add_edit_task.html', {'form': form, 'title': 'Редактировать задачу'})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        messages.success(request, "Задача успешно удалена.")
+        return redirect('task_list')
+    return render(request, 'tasks/confirm_delete.html', {'task': task})
